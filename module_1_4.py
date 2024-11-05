2e899aad56e7bb80a15d88067c2610935941ed13
zinnurch <zinnurch7@gmail.com>
2024-11-06 00:42:57 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string=input("Произвольный текс: ")
+number=17
+print(number)
+print(my_string.upper())
+print(my_string.lower())
+print("произвольный текст".replace(" ",''))
+print(my_string[0])
+print(my_string[-1])
