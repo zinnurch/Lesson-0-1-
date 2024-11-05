fbc85cac8982e77cb9fd632404bb6cab71d75b6a
zinnurch <zinnurch7@gmail.com>
2024-11-06 01:35:05 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_tuple=(1, 2, 'a', 'b')
+print(immutable_tuple)
+#immutable_tuple[0]=2
+#print(immutable_tuple)
+mutable_list=([1,3,5],'cat')
+print(mutable_list)
+mutable_list[0][1]=7
+print(mutable_list)
