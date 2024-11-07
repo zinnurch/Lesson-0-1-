fb968ae61c8dcd4268f90251ce597f7a52191e56
zinnurch <zinnurch7@gmail.com>
2024-11-08 01:34:51 +0700

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict={"zinnur": 1989}
+print(my_dict)
+print(my_dict["zinnur"])
+my_dict["anna"]=1990
+print(my_dict["anna"])
+my_dict.update({"allan": 1985, "ken": 2001})
+print(my_dict)
+del(my_dict['ken'])
+print(my_dict)
+my_set={1,2,3,7,7,5,3,"cat"}
+print(my_set)
+my_set.update({2024,'dog'})
+print(my_set)
+print(my_set.discard(2024))
+print(my_set)
