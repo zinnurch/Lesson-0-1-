6eb0e2b802b7b089ed4c38d7397b34ad9b1a977c
zinnurch <zinnurch7@gmail.com>
2024-11-20 00:05:31 +0700

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+numbers = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+b = len(numbers)
+while a < b:
+    if numbers [a] == 0:
+        a = a + 1
+        continue
+    elif numbers [a] > 0:
+        print(numbers[a])
+        a = a + 1
+    elif numbers [a] < 0:
+        break
+
